45b1f4a8be722d90d536793b36ceb40d7b4c0524
Vladimir <abuhnoaeivladimir@gmail.com>
2019-02-20 20:52:32 +0200

    added Material UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { TextField, Button } from '@material-ui/core';
 
 class App extends Component {
+  state = {
+    searchTerm: ''
+  }
+  onTextChange = (event) => {
+    const value = event.target.value;
+    this.setState({ searchTerm: value });
+  }
+
+  onSearchClick = () => {
+    console.log(this.state.searchTerm);
+    this.setState({ searchTerm: '' })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+<<<<<<< HEAD
+          <AppBar position="static" color="primary">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">
+                Photos
+          </Typography>
+              <TextField
+                placeholder="Search on spotify"
+                onChange={this.onTextChange}
+                value={this.state.searchTerm}
+              />
+              <Button
+                onClick={this.onSearchClick} disabled={this.state.searchTerm.length ===0}
+              >
+                Search
+              </Button>
+            </Toolbar>
+          </AppBar>
+=======
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -19,8 +53,13 @@ class App extends Component {
           >
             Learn React
           </a>
+>>>>>>> 557356475bf6a7bc2028627784394e61e6302d3a
         </header>
+        <div>
+
+        </div>
       </div>
+
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+<<<<<<< HEAD
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+=======
+>>>>>>> 557356475bf6a7bc2028627784394e61e6302d3a
